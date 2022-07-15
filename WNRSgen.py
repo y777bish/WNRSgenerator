0bb2b277c42af8af4d3249f88a649696a84a2f8f
Bartosz Smolarz <bartsmo430@student.polsl.pl>
2022-07-15 19:50:16 +0200

    some allignment, colour and visuals

--- WNRSgen.py
+++ WNRSgen.py
@@ -1,6 +1,22 @@
 from docx import Document
 from docx.shared import Inches
+from docx.shared import RGBColor
+from docx.shared import Pt
+from docx.shared import Cm
 from docx.enum.table import WD_TABLE_ALIGNMENT
+from docx.enum.table import WD_ALIGN_VERTICAL
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+
+def fillincells():
+    for k in range(len(recfixed)):
+        row = table.add_row().cells
+        for cells in table.rows[-1].cells:
+            cells.text = lines[k]
+            run = cells.paragraphs[0].runs[0]  # formatting occurs at run level, I'll call it as real-time
+            run.font.bold = True
+            run.font.name = 'Calibri'
+            run.font.size = Pt(24)
+            run.font.color.rgb = RGBColor(0x7E, 0x35, 0x17)
 
 lines = []
 with open('wnrslines.txt','r') as f:
@@ -24,9 +40,19 @@ for i in range(len(recfixed)):
 table = document.add_table(rows=0, cols=2)
 table.alignment = WD_TABLE_ALIGNMENT.CENTER
 table.style = 'Table Grid'
-for k in range(len(recfixed)):
-    table.add_row()
-    for cells in table.rows[-1].cells:
-        cells.text = lines[k]
+# paragraph = document.add_paragraph()
+# paragraph_format = paragraph.paragraph_format
+# paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+fillincells()
+for row in table.rows:
+    row.height = Cm(5.6)
+
+table.cell(0, 0).paragraphs[0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER
+table.cell(0, 1).paragraphs[0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER
+table.cell(1, 0).paragraphs[0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER
+table.cell(1, 1).paragraphs[0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER #horizontal allignment
+
+table.cell(1, 1).vertical_alignment = WD_TABLE_ALIGNMENT.CENTER #vertical allignment
 
 document.save('WNRSprint.docx')
